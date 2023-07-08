b5d855ada631a9ad03cb0041000b9c5650e0b33c
[Araind Kumar] <[20r15a0510@gcet.edu.in]>
2023-07-08 23:39:38 +0530

    pro5added

--- src/components/project_cardss/myprojects.js
+++ src/components/project_cardss/myprojects.js
@@ -4,6 +4,7 @@ import pro1 from '../asessts/gtp3.jpg'
 import pro2 from '../asessts/mymoviebook.jpg'
 import pro3 from '../asessts/zomatolanding.jpg'
 import pro4 from '../asessts/dragon.jpg'
+import pro5 from '../asessts/todolsit.jpg'
 
 
 const myprojects = () => {
@@ -46,6 +47,16 @@ const myprojects = () => {
     <div className='btn1'><a href='https://dragongameusingjavascript.netlify.app/'>view</a> </div>
     <div className='btn1'><a href='https://github.com/AravindKumar-1431/Dragon-game'>Source</a></div>
     </div>
+
+     <div className="project-card5"><img className='imga' src={pro5} alt="image5"/>
+    <h2>TO-DO-LIST</h2>
+    <h5>this web app is designed by using REACTJS</h5>
+    <div className='btn1'><a href="https://mytodolsitapp.netlify.app/">view</a> </div>
+    <div className='btn1'><a href='https://github.com/AravindKumar-1431/my-to-do-list'>Source</a></div>
+    </div>
+
+
+
 
     </div>
 
