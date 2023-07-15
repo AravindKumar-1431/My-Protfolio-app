3190ad963c23c34997e7ac7ebfbde7a32bfe1ba0
[Araind Kumar] <[20r15a0510@gcet.edu.in]>
2023-07-15 16:55:00 +0530

    project cardedit

--- src/components/project_cardss/myprojects.js
+++ src/components/project_cardss/myprojects.js
@@ -7,7 +7,33 @@ import pro4 from '../asessts/dragon.jpg'
 import pro5 from '../asessts/todolsit.jpg'
 
 
-const myprojects = () => {
+const myprojects = (props) => {
+
+  const projectcards=[
+    {
+    id:1,
+    name:'GPT3',
+    Image:pro1,
+    content:'This Website is desigend  by using Reactjs modern ui/ux design ',
+    btn1: <a href='https://gpt3-uiux.netlify.app/'>view</a>,
+    btn2:<a href='https://github.com/AravindKumar-1431/GPT-3Fully-Responsive-Modern-UI-UX-Website-in-React-JS'>Source</a>
+  },
+  {
+    id:2,
+    name:'Book Your Shows',
+    Image:pro2,
+    content:'this website is designed to Book your Movie Ticket Easily ',
+    btn1: <a href='https://mymoviebookingapp.netlify.app/'>view</a>,
+    btn2:<a href='https://github.com/AravindKumar-1431/Movie-Ticket-Booking-App'>Source</a>
+  },
+  {
+    id:3,
+    name:'Zomato landing page',
+    Image:pro3,
+    content:"this web app is designed by using pure HTML and CSS",
+    btn1: <a href='https://myzomatolandingpage.netlify.app/'>view</a>,
+    btn2:<a href='https://github.com/AravindKumar-1431/Zomato-landing-page'>Source</a>}
+]
   return (
 
     
@@ -16,28 +42,21 @@ const myprojects = () => {
     <div><h1 className='project-heading'>PROJECTS</h1> </div>
 
     <div className="project-container">
-    
-    <div className="project-card1"><img className='imga' src={pro1} alt="image1"/>
-    <h2>GPT3</h2>
-    <h5>This Website is desigend  by using Reactjs modern ui/ux design </h5>
-    
-    <div className='btn1'><a href='https://gpt3-uiux.netlify.app/'>view</a> </div>
-    <div className='btn1'><a href='https://github.com/AravindKumar-1431/GPT-3Fully-Responsive-Modern-UI-UX-Website-in-React-JS'>Source</a></div>
-    </div>
 
-    <div className="project-card2"><img className='imga' src={pro2} alt="image2"/>
-    <h2>Movie Ticket Booking</h2>
-    <h5>It is movie ticket booking web App using Reactjs. And it is a Full Stack project</h5>
-    <div className='btn1'><a href='https://mymoviebookingapp.netlify.app/'>view</a> </div>
-    <div className='btn1'><a href='https://github.com/AravindKumar-1431/Movie-Ticket-Booking-App'>Source</a></div>
-   </div>
-
-    <div className="project-card3"><img className='imga' src={pro3} alt="image3"/>
-    <h2>Zomato landing page</h2>
-    <h5>this web app is designed by using pure HTML and CSS</h5>
-    <div className='btn1'><a href='https://myzomatolandingpage.netlify.app/'>view</a> </div>
-    <div className='btn1'><a href='https://github.com/AravindKumar-1431/Zomato-landing-page'>Source</a></div>
+    
+    {projectcards.map((project)=>(
+    <div className="project-card1" key={project.id}>
+    <img className='imga' src={project.Image} alt="image1"/>
+    <h2>{project.name}</h2>
+    <h5>{project.content}</h5>
+    
+    <div className='btn1'>{project.btn1}</div>
+    <div className='btn1'>{project.btn2}</div>
     </div>
+    ))}
+
+
+
 
     
 
